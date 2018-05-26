dfdef941ef27c31684a1e6d271ec5697c74da850
luolijun <libbylg@126.com>
2018-05-26 23:42:08 +0800

    merge brfore

--- mo.c
+++ mo.c
@@ -1,98 +1,169 @@
 #include "mo.h"
 
 
+struct stream_t
+{
+    void*               ctx;
+    MO_READ_CALLBACK    read;
+
+    struct stream_t*    parent;
+};
+
 struct lex_t
 {
     void*               ctx;
-    struct lex_t*       parent;
-    struct result_t*    result;
-    mo_u32              (*next)(void* ctx, struct lex_t*  x, struct token_t* t);
+    MO_NEXT_CALLBACK    next;
+
+    struct stream_t*    stream_top;
+    struct token_t*     token;
 };
 
 struct unit_t
 {
     void*               ctx;
+    MO_ACCEPT_CALLBACK  accept;
+
     struct lex_t*       parent;
-    struct result_t*    result;
-    mo_u32              (*accept)(void* ctx, struct unit_t* x, struct token_t* t)
 };
 
-struct mo_t
+struct sytx_t
 {
-    void*               ctx;
-    struct lex_t*       lex;
-    struct unit_t*      unit;
-    struct result_t*    result;
+    struct unit_t*      unit_top;
 };
 
-
-
-
-MO_EXTERN   struct mo_t*        mo_new(void* ctx, struct lex_t* x, struct unit_t* u, struct result_t* r)
-{
-    struct mo_t* m = (struct mo_t*)malloc(sizeof(struct mo_t));
-    if (NULL == m)
-    {
-        return NULL;
-    }
-
-    m->lex    = x;
-    m->unit   = u;
-    m->result = r;
-    m->ctx    = ctx;
-    return m;
-}
-
-
-
-
-MO_EXTERN   struct lex_t*       mo_lex_new (void* ctx, mo_u32 (*next  )(void* ctx, struct lex_t*  x, struct token_t* t), struct result_t* r)
-{
-    struct lex_t* x = (struct lex_t*)malloc(sizeof(struct lex_t));
-    if (NULL == x)
-    {
-        return NULL;
-    }
-
-    x->parent = NULL;
-    x->ctx    = ctx;
-    x->next   = next;
-    x->result = r;
-    return x;
-}
-
-
-
-
-MO_EXTERN   struct unit_t*      mo_unit_new(void* ctx, mo_u32 (*accept)(void* ctx, struct unit_t* x, struct token_t* t), struct result_t* r)
+MO_EXTERN   struct unit_t*      mo_unit_new     (void* ctx, MO_ACCEPT_CALLBACK accept)
 {
     struct unit_t* u = (struct unit_t*)malloc(sizeof(struct unit_t));
-    if (NULL == u)
-    {
-        return NULL;
-    }
-
-    u->parent = NULL;
-    u->ctx    = ctx;
-    u->accept = accept;
-    u->result = r;
+    u->ctx        = ctx;
+    u->accept     = accept;
+    u->parent     = NULL;
     return u;
 }
 
+MO_EXTERN   void                mo_unit_del     (struct unit_t* u)
+{
+    if (NULL == u)
+    {
+        return;
+    }
+
+    free(u);
+    return;
+}
+
+MO_EXTERN   struct stream_t*    mo_stream_new   (void* ctx, MO_READ_CALLBACK   read)
+{
+    struct stream_t* m = (struct stream_t*)malloc(sizeof(struct stream_t));
+    m->ctx        = ctx;
+    m->read       = read;
+    m->parent     = NULL;
+    return m;
+}
+
+MO_EXTERN   void                mo_stream_del   (struct stream_t* m)
+{
+    if (NULL == m)
+    {
+        return;
+    }
+
+    free(m);
+    return;
+}
+
+MO_EXTERN   struct lex_t*       mo_lex_new      (void* ctx, MO_NEXT_CALLBACK   next)
+{
+    struct lex_t* x = (struct lex_t*)malloc(sizeof(struct stream_t));
+    x->ctx        = ctx;
+    x->next       = next;
+    x->stream_top = NULL;
+    return x;
+}
+
+MO_EXTERN   void                mo_lex_del      (struct lex_t* x)
+{
+    if (NULL == x)
+    {
+        return;
+    }
+
+    free(x);
+    return;
+}
+
+MO_EXTERN   void                mo_lex_put      (struct lex_t* x, struct stream_t* m)
+{
+    m->parent     = x->stream_top;
+    x->stream_top = m;
+}
+
+MO_EXTERN   struct sytx_t*      mo_sytx_new     ()
+{
+    struct sytx_t* y = (struct sytx_t*)malloc(sizeof(struct sytx_t));
+    y->unit_top = NULL;
+    return y;
+}
+
+MO_EXTERN   void                mo_sytx_del     (struct sytx_t* y)
+{
+    if (NULL == y)
+    {
+        return;
+    }
+
+    free(y);
+}
+
+MO_EXTERN   void                mo_sytx_push    (struct sytx_t* y, struct unit_t* u)
+{
+    u->parent = y->unit_top;
+    y->unit_top = u;
+}
+
+MO_EXTERN   struct unit_t*      mo_sytx_pop     (struct sytx_t* y)
+{
+    struct unit_t* u = y->unit_top;
+    if (NULL != y->unit_top)
+    {
+        y->unit_top = y->unit_top->parent;
+    }
+
+    return u;
+}
+
+MO_EXTERN   struct unit_t*      mo_sytx_top     (struct sytx_t* y)
+{
+    return y->unit_top;
+}
+
+MO_EXTERN   int                 mo_walk(struct sytx_t* y, struct lex_t* x)
+{
+    while (1)
+    {
+        mo_token id = (*(x->next))(x->ctx, x, x->token);
+        if (MO_TOKEN_ERROR == id)
+        {
+            return 111;
+        }
+
+        if (MO_TOKEN_EOF == id)
+        {
+
+        }
+
+RETRY:
+        mo_action action = (y->unit_top->accep)(y->unit_top->ctx, y, x->token);
+        if (MO_ACTION_NEEDMORE == action)
+        {
+            continue;
+        }
+
+        if (MO_ACTION_TRYAGAIN == action)
+        {
+            goto RETRY;   
+        }
+    }
+}
 
 
 
-MO_EXTERN   struct result_t*    mo_result(struct unit_t* x);
-MO_EXTERN   struct result_t*    mo_lex_result (struct lex_t*  x);
-MO_EXTERN   struct result_t*    mo_unit_result(struct unit_t* x);
-
-
-
-MO_EXTERN   void                mo_setup_result(struct mo_t* m, struct result_t* r);
-MO_EXTERN   struct unit_t*      mo_compile(struct mo_t* m, void* ctx, int (*read)(char* data, int* len));
-MO_EXTERN   void                mo_del(struct mo_t* m);
-
-
-
-MO_EXTERN   struct mo_t*        mo_push(struct mo_t* m, struct unit_t* u);
-MO_EXTERN   struct unit_t*      mo_pop(struct mo_t* m);
--- mo.h
+++ mo.h
@@ -10,52 +10,62 @@
 
 
 
-typedef unsigned int    mo_u32;
+
 typedef int             mo_errno;
-typedef int             mo_bool;
-#define MO_TRUE    (1)
-#define MO_FALSE   (0)
+typedef unsigned int    mo_token;
+typedef int             mo_action;
+// typedef int             mo_bool;
+// #define MO_TRUE    (1)
+// #define MO_FALSE   (0)
 
 
 struct lex_t;
+struct sytx_t;
 struct unit_t;
-struct mo_t;
+struct feed_t;
 struct result_t;
 struct token_t;
 
 
 struct token_t
 {
-    mo_u32  type;
-    char*   text[2];
-};
-
-struct result_t
-{
-    mo_errno    id;
-    char        error[256];
+    mo_token    token;      ///<    符号的编号
+    char*       text[2];    ///<    符号的文本范围
 };
 
 
-#define     MO_UNIT_ERROR   (0)
-#define     MO_UNIT_MORE    (1)
-#define     MO_UNIT_AGAIN   (2)
+#define     MO_ACTION_ERROR     (0)
+#define     MO_ACTION_NEEDMORE  (1)
+#define     MO_ACTION_TRYAGAIN  (2)
+
+#define     MO_TOKEN_ERROR      (0)
+#define     MO_TOKEN_EOF        (0xFFFFFFFF)
+
+typedef     int       (*MO_READ_CALLBACK  )(void* ctx, char* buf, int* len);
+typedef     mo_token  (*MO_NEXT_CALLBACK  )(void* ctx, struct lex_t*  x, struct token_t* t);
+typedef     mo_action (*MO_ACCEPT_CALLBACK)(void* ctx, struct sytx_t* y, struct token_t* t);
+
+MO_EXTERN   struct unit_t*      mo_unit_new     (void* ctx, MO_ACCEPT_CALLBACK accept);
+MO_EXTERN   void                mo_unit_del     (struct unit_t* u);
+
+MO_EXTERN   struct stream_t*    mo_stream_new   (void* ctx, MO_READ_CALLBACK   read);
+MO_EXTERN   void                mo_stream_del   (struct stream_t* u);
+
+MO_EXTERN   struct lex_t*       mo_lex_new      (void* ctx, MO_NEXT_CALLBACK   next);
+MO_EXTERN   void                mo_lex_del      (struct lex_t* x);
+MO_EXTERN   void                mo_lex_put      (struct lex_t* x, struct stream_t* m);
+
+MO_EXTERN   struct sytx_t*      mo_sytx_new     ();
+MO_EXTERN   void                mo_sytx_del     (struct sytx_t* y);
+MO_EXTERN   void                mo_sytx_push    (struct sytx_t* y, struct unit_t* u);
+MO_EXTERN   struct unit_t*      mo_sytx_pop     (struct sytx_t* y);
+MO_EXTERN   struct unit_t*      mo_sytx_top     (struct sytx_t* y);
+
+MO_EXTERN   int                 mo_walk(struct sytx_t* y, struct lex_t* x);
 
 
-MO_EXTERN   struct mo_t*        mo_new(void* ctx, struct lex_t* x, struct unit_t* u, struct result_t* r);
-MO_EXTERN   struct lex_t*       mo_lex_new (void* ctx, mo_u32 (*next  )(void* ctx, struct lex_t*  x, struct token_t* t), struct result_t* r);
-MO_EXTERN   struct unit_t*      mo_unit_new(void* ctx, mo_u32 (*accept)(void* ctx, struct unit_t* x, struct token_t* t), struct result_t* r);
 
 
-
-MO_EXTERN   struct unit_t*      mo_compile(struct mo_t* m, void* ctx, int (*read)(char* data, int* len));
-MO_EXTERN   void                mo_del(struct mo_t* m);
-
-
-
-
-MO_EXTERN   struct mo_t*        mo_push(struct mo_t* m, struct unit_t* u);
-MO_EXTERN   struct unit_t*      mo_pop(struct mo_t* m);
 
 
 
