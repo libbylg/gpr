8e1c0ac7f7412fec2c072f2f3a24003efe86bfed
luolijun <libbylg@126.com>
2018-06-04 08:09:36 +0800

    lex impl

--- plugins/plugins-protobuf/protobuf_lex.c
+++ plugins/plugins-protobuf/protobuf_lex.c
@@ -36,6 +36,7 @@ MO_EXTERN   struct protobuf_lex_t* protobuf_lex_new(int init_cache_size)
 static int       protobuf_lex_locate(struct lex_t*  x)
 {
 RETRY:
+    register struct cache_t*    cache   = x->cache_top;
     register struct stream_t*   stream  = x->cache_top->stream;
     register char*              pc      = x->cache_top->pc;
     register char*              pe      = x->cache_top->pe;
@@ -83,8 +84,19 @@ RETRY:
 
 
 
+static mo_token  protobuf_lex_setup_token(struct lex_t*    x, struct token_t* t, int size, mo_token token)
+{
+    t->token = token;
+    x->cache_top->pc += size;
+    return token;
+}
+
+
+
+
 static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
 {
+RETRY:
     ///!    已经没有任何数据可以读取了
     if (NULL == x->cache_top)
     {
@@ -102,13 +114,6 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
 
         if (MO_READ_EOF == ret)
         {
-            ///!    cache退栈
-            struct cache_t* cache = x->cache_top;
-            x->cache_top = x->cache_top->prev;
-            (*(cache->del))(cache);
-    
-            ///!    开始从下一个cache定位首个有效字符位置
-            ret = protobuf_lex_locate(x->cache_top);
             return MO_TOKEN_EOF;
         }
     }
@@ -117,16 +122,11 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
     register char* pc = x->cache_top->pc;
     switch (*pc)
     {
-    case '/':   return  '/';
-    case '{':   return  '{';
-    case '}':   return  '}';
-    case '(':   return  '(';
-    case ')':   return  ')';
-    case ';':   return  ';';
-    case ',':   return  ',';
-    case '=':   return  '=';
-    case '<':   return  '<';
-    case '>':   return  '>';
+    case '{':  case '}':  case  '(':  case ')':  case  ';':
+    case ',':  case '=':  case  '<':  case '>':  
+        {
+            return protobuf_lex_setup_token(x, t, 1, *pc);   
+        }
     case 'e':   //  enum    exit    extensions  extend
         {
             if ( ('n' == pc[1])  
@@ -134,7 +134,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('m' == pc[3]) 
             &&   (0 == (cm[pc[4]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_enum;
+                return protobuf_lex_setup_token(x, t, 4, MO_TOKEN_enum);
             }
 
             if ( ('x' == pc[1])  
@@ -142,7 +142,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('t' == pc[3]) 
             &&   (0 == (cm[pc[4]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_exit;
+                return protobuf_lex_setup_token(x, t, 4,  MO_TOKEN_exit);
             }
 
             if ( ('x' == pc[1])  
@@ -152,7 +152,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('d' == pc[5]) 
             &&   (0 == (cm[pc[6]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_extend;
+                return protobuf_lex_setup_token(x, t, 6,  MO_TOKEN_extend);
             }
 
             if ( ('x' == pc[1])  
@@ -166,7 +166,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('s' == pc[9]) 
             &&   (0 == (cm[pc[10]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_extensions;
+                return protobuf_lex_setup_token(x, t, 10,  MO_TOKEN_extensions);
             }
 
             return protobuf_try_name();
@@ -178,7 +178,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('t' == pc[3]) 
             &&   (0 == (cm[pc[4]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_init;
+                return protobuf_lex_setup_token(x, t, 4,  MO_TOKEN_init);
             }
 
             if ( ('m' == pc[1])  
@@ -188,7 +188,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('t' == pc[5]) 
             &&   (0 == (cm[pc[6]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_import;
+                return protobuf_lex_setup_token(x, t, 6,  MO_TOKEN_import);
             }
 
             return protobuf_try_name();
@@ -199,7 +199,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('p' == pc[2])  
             &&   (0 == (cm[pc[3]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_map;
+                return protobuf_lex_setup_token(x, t, 3,  MO_TOKEN_map);
             }
 
             if ( ('e' == pc[1])  
@@ -210,7 +210,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('e' == pc[6]) 
             &&   (0 == (cm[pc[7]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_message;
+                return protobuf_lex_setup_token(x, t, 7,  MO_TOKEN_message);
             }
 
             return protobuf_try_name();
@@ -223,7 +223,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('f' == pc[4])  
             &&   (0 == (cm[pc[5]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_oneof;
+                return protobuf_lex_setup_token(x, t, 5,  MO_TOKEN_oneof);
             }
 
             if ( ('p' == pc[1])  
@@ -235,7 +235,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('l' == pc[7]) 
             &&   (0 == (cm[pc[8]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_optional;
+                return protobuf_lex_setup_token(x, t, 8,  MO_TOKEN_optional);
             }
 
             if ( ('p' == pc[1])  
@@ -243,9 +243,9 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('i' == pc[3]) 
             &&   ('o' == pc[4]) 
             &&   ('n' == pc[5]) 
-            &&   (0 == (cm[pc[8]]&(CM_ALPHA|CM_DEC))) )
+            &&   (0 == (cm[pc[6]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_option;
+                return protobuf_lex_setup_token(x, t, 6,  MO_TOKEN_option);
             }
 
             return protobuf_try_name();   
@@ -260,7 +260,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('e' == pc[6]) 
             &&   (0 == (cm[pc[7]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_package;
+                return protobuf_lex_setup_token(x, t, 7,  MO_TOKEN_package);
             }
 
             return protobuf_try_name();   
@@ -276,7 +276,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('d' == pc[7]) 
             &&   (0 == (cm[pc[8]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_repeated;
+                return protobuf_lex_setup_token(x, t, 8,  MO_TOKEN_repeated);
             }
 
             if ( ('e' == pc[1])  
@@ -288,7 +288,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('d' == pc[7]) 
             &&   (0 == (cm[pc[8]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_required;
+                return protobuf_lex_setup_token(x, t, 8,  MO_TOKEN_required);
             }
 
             if ( ('e' == pc[1])  
@@ -300,7 +300,7 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('d' == pc[7]) 
             &&   (0 == (cm[pc[8]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_reserved;
+                return protobuf_lex_setup_token(x, t, 8,  MO_TOKEN_reserved);
             }
 
             if ( ('e' == pc[1])  
@@ -311,14 +311,14 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('s' == pc[6]) 
             &&   (0 == (cm[pc[7]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_returns;
+                return protobuf_lex_setup_token(x, t, 7,  MO_TOKEN_returns);
             }
 
             if ( ('p' == pc[1])  
             &&   ('c' == pc[2])  
             &&   (0 == (cm[pc[3]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_rpc;
+                return protobuf_lex_setup_token(x, t, 3,  MO_TOKEN_rpc);
             }
 
             return protobuf_try_name();   
@@ -333,26 +333,70 @@ static mo_token  protobuf_lex_next(struct lex_t*    x, struct token_t* t)
             &&   ('e' == pc[6])  
             &&   (0 == (cm[pc[7]]&(CM_ALPHA|CM_DEC))) )
             {
-                return MO_TOKEN_service;
+                return protobuf_lex_setup_token(x, t, 7,  MO_TOKEN_service);
             }
 
             return protobuf_try_name();   
         }
-    case 'a':  case 'b':  case 'c':  case 'd':/*case 'e':*/case 'f': 
-    case 'g':  case 'h':  case 'j':/*case 'i':*/case 'k':  case 'l': 
-  /*case 'm':*/case 'n':/*case 'o':  case 'p':*/case 'q':/*case 'r':*/
-  /*case 's':*/case 't':  case 'u':  case 'v':  case 'w':  case 'x': 
-    case 'y':  case 'z':
-    
-    case 'A':  case 'B':  case 'C':  case 'D':  case 'E':  case 'F': 
-    case 'G':  case 'H':  case 'J':  case 'I':  case 'K':  case 'L': 
-    case 'M':  case 'N':  case 'O':  case 'P':  case 'Q':  case 'R': 
-    case 'S':  case 'T':  case 'U':  case 'V':  case 'W':  case 'X': 
-    case 'Y':  case 'Z':
+    case 'a':  case 'b':  case 'c':  case 'd':/*case 'e':*/
+    case 'f':  case 'g':  case 'h':  case 'j':/*case 'i':*/
+    case 'k':  case 'l':/*case 'm':*/case 'n':/*case 'o':*/
+  /*case 'p':*/case 'q':/*case 'r':  case 's':*/case 't':  
+    case 'u':  case 'v':  case 'w':  case 'x':  case 'y':  
+    case 'z':
+    case 'A':  case 'B':  case 'C':  case 'D':  case 'E':  
+    case 'F':  case 'G':  case 'H':  case 'J':  case 'I':  
+    case 'K':  case 'L':  case 'M':  case 'N':  case 'O':  
+    case 'P':  case 'Q':  case 'R':  case 'S':  case 'T':  
+    case 'U':  case 'V':  case 'W':  case 'X':  case 'Y':  
+    case 'Z':
+    case '_':
+        return protobuf_try_name();   
+    case '0':  case '1':  case '2':  case '3':  case '4':  
+    case '5':  case '6':  case '7':  case '8':  case '9':
+        return protobuf_try_number();   
+    case '"':
+        return protobuf_try_string();
+    case '/':
+        protobuf_try_comment();
+        goto RETRY; 
+    default:
+        mo_push_result(mo_result_new("lex", 111, "unrecogenized char '0x%.2D'", (unsigned int)(*pc)));
+        return MO_TOKEN_ERROR;
     }
+}
+
+
+
+
+static mo_token  protobuf_try_name      (struct lex_t*    x, struct token_t* t)
+{
 
 }
 
+
+
+
+static mo_token  protobuf_try_number    (struct lex_t*    x, struct token_t* t)
+{
+
+}
+
+
+
+
+static mo_token  protobuf_try_string    (struct lex_t*    x, struct token_t* t)
+{
+
+}
+
+
+
+
+static mo_token  protobuf_try_comment   (struct lex_t*    x, struct token_t* t)
+{
+
+}
 
 
 
--- plugins/plugins-protobuf/protobuf_token.h
+++ plugins/plugins-protobuf/protobuf_token.h
@@ -16,36 +16,41 @@ struct protobuf_token_t
 
 
 ///!    所有的符号定义，如果是单字符符号，那么直接用字符本身的字面量代替
-//                                  //  {
-//                                  //  }
-//                                  //  (
-//                                  //  )
-//                                  //  ;
-//                                  //  ,
-//                                  //  =
-//                                  //  <
-//                                  //  >
-#define MO_TOKEN_STRING     200     //  "xxxx"
-#define MO_TOKEN_INTEGER    201     //  12345
-#define MO_TOKEN_message    301     //  message
-#define MO_TOKEN_service    301     //  service
-#define MO_TOKEN_required   302     //  required
-#define MO_TOKEN_optional   303     //  optional
-#define MO_TOKEN_repeated   304     //  repeated
-#define MO_TOKEN_rpc        305     //  rpc
-#define MO_TOKEN_init       306     //  init
-#define MO_TOKEN_exit       307     //  exit
-#define MO_TOKEN_reserved   308     //  reserved
-#define MO_TOKEN_enum       309     //  enum
-#define MO_TOKEN_import     310     //  import
-#define MO_TOKEN_extensions 311     //  extensions
-#define MO_TOKEN_extend     312     //  extend
-#define MO_TOKEN_oneof      313     //  oneof
-#define MO_TOKEN_map        314     //  map
-#define MO_TOKEN_package    315     //  package
-#define MO_TOKEN_option     316     //  option
-#define MO_TOKEN_returns    317     //  returns
-#define MO_TOKEN_ERROR      999     //  遇到错误
+//  下面都是单符号分隔符
+//                                      //  {
+//                                      //  }
+//                                      //  (
+//                                      //  )
+//                                      //  ;
+//                                      //  ,
+//                                      //  =
+//                                      //  <
+//                                      //  >
+//  下面都是关键字
+#define MO_TOKEN_message        301     //  message
+#define MO_TOKEN_service        301     //  service
+#define MO_TOKEN_required       302     //  required
+#define MO_TOKEN_optional       303     //  optional
+#define MO_TOKEN_repeated       304     //  repeated
+#define MO_TOKEN_rpc            305     //  rpc
+#define MO_TOKEN_init           306     //  init
+#define MO_TOKEN_exit           307     //  exit
+#define MO_TOKEN_reserved       308     //  reserved
+#define MO_TOKEN_enum           309     //  enum
+#define MO_TOKEN_import         310     //  import
+#define MO_TOKEN_extensions     311     //  extensions
+#define MO_TOKEN_extend         312     //  extend
+#define MO_TOKEN_oneof          313     //  oneof
+#define MO_TOKEN_map            314     //  map
+#define MO_TOKEN_package        315     //  package
+#define MO_TOKEN_option         316     //  option
+#define MO_TOKEN_returns        317     //  returns
+#define MO_TOKEN_RESERVED_MAX   399     //  -------
+//  下面都是字面量或者用户定义的名字
+#define MO_TOKEN_STRING         400     //  "xxxx"
+#define MO_TOKEN_INTEGER        401     //  12345
+#define MO_TOKEN_NAME           402     /// 一个名字
+
 
     //      double
     //      float
